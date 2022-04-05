e246d43080d298469347fd9b40b7d4475ab5c818
Andres Ferro <andresferro6@hotmail.com>
2022-04-04 21:09:32 -0500

    Primer commit, estructura inicial del proyecto

---
+++ main.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  4 21:07:55 2022
+
+@author: ASUS
+"""
+
---
+++ settings.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  4 21:08:03 2022
+
+@author: ASUS
+"""
+
